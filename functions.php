9e5be09cceeecee54b7076b010d161e2b9cfa680
KilliannH <killiannhervagault@gmail.com>
2019-07-01 10:30:39 +0200

    added bootstrap 4

--- functions.php
+++ functions.php
@@ -1,5 +1,9 @@
 <?php
 function add_theme_scripts() {
+	wp_enqueue_style( 'bootstrap', get_template_directory_uri() . '/vendors/bootstrap/css/bootstrap.css' );
+	wp_enqueue_script( 'jquery', get_template_directory_uri() . '/vendors/jquery/jquery-3.4.1.min.js' );
+	wp_enqueue_script( 'popper', get_template_directory_uri() . '/vendors/popper/popper.min.js' );
+	wp_enqueue_script( 'bootstrap', get_template_directory_uri() . '/vendors/bootstrap/js/bootstrap.js' );
 	wp_enqueue_style( 'style', get_stylesheet_uri() );
 
 	wp_enqueue_script( 'app', get_template_directory_uri() . '/js/app.js');
--- header.php
+++ header.php
@@ -12,3 +12,19 @@
 </head>
 
 <body>
+
+<nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark under-the-wp-nav">
+    <a class="navbar-brand" href="#"><?php echo get_bloginfo('name') ?></a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarCollapse">
+        <ul class="navbar-nav ml-auto">
+            <li class="nav-item active">
+                <a class="nav-link" href="#">Songs <span class="sr-only">(current)</span></a>
+            </li>
+        </ul>
+    </div>
+</nav>
+
+<div class="container under-the-nav">
